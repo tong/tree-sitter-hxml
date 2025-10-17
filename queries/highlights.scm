52f90a65b70fa24b9c3260512ffccba509ac2d5f
tong <tong@disktree.net>
2025-10-17 20:14:08 +0200

    cleanup highlights

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,64 +1,58 @@
+; Comments
 (comment) @comment
 
-;;; Keywords & Flags
-
-;; Simple flags without arguments
-;;
+; Keywords / Flags
 (flag) @keyword
 
-;; Flags with arguments
-;;
-(class_path) @keyword
-(cmd) @keyword
-(connect) @keyword
-(cwd) @keyword
-(dce) @keyword
-(define) @keyword
-(hxb) @keyword
-(hxb_lib) @keyword
-(library) @keyword
-(macro) @keyword
-(main) @keyword
-(remap) @keyword
-(resource) @keyword
-(server_connect) @keyword
-(server_listen) @keyword
-(target) @keyword
-(json_output) @keyword
-(xml_output) @keyword
+; Flags with arguments
+[
+  (class_path)
+  (cmd)
+  (connect)
+  (cwd)
+  (dce)
+  (define)
+  (hxb)
+  (hxb_lib)
+  (library)
+  (macro)
+  (main)
+  (remap)
+  (resource)
+  (server_connect)
+  (server_listen)
+  (target)
+  (json_output)
+  (xml_output)
+] @keyword
 
-; (next) @keyword
+; Constants & Modes
+[
+  ; (constant)
+  ; (library_spec)
+  (dce_mode)
+  (custom_target)
+] @constant
 
-;; Literals
-;;
-(constant) @constant
-(library_spec) @constant
-(dce_mode) @constant
-(custom_target) @constant
-(net_address) @string
-(server_address) @string
+; Strings / Paths
+[
+  (file)
+  (directory)
+  (include)
+  (command)
+  (argument)
+  (net_address)
+  (server_address)
+] @string
 
-;; Paths and Types
-;;
-(file) @string
-(directory) @string
-(include) @string ; hxml file inclusion
+; Names & Types
 (package) @namespace
 (type_path) @type
-(package) @namespace
 (resource_name) @property
 
-;; Other arguments
-;;
-(command) @string
-(argument) @string
+; Macro expressions
+(macro (expr) @function)
 
-;; Macro calls
-;;
-(macro
-  (expr) @function)
-
-;; Highlight the constant within a define statement
-;;
-(define
-  (constant) @constant)
+; defines
+(define_key) @constant
+(define_value) @string
