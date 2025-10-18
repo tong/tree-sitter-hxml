37b209a8c6104131153e6b12dd22dc398ce00d16
tong <tong@disktree.net>
2025-10-18 18:16:50 +0200

    update injections.scm

--- queries/injections.scm
+++ queries/injections.scm
@@ -1,8 +1,6 @@
-; Inject Haxe inside --macro expressions
-(
-  (macro
-    (expr) @injection.content)
+; Inject Haxe inside --macro <expressions>
+((macro
+  (expr) @injection.content)
   ; Skip if the expression starts with a single or double quote
   (#not-match? @injection.content "^['\\\"]")
-  (#set! injection.language "haxe")
-)
+  (#set! injection.language "haxe"))
