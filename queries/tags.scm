f5f747d5631e1b17c94d44c0d1304cc637d1c87c
tong <tong@disktree.net>
2025-10-31 19:50:06 +0100

    add tags.scm

---
+++ queries/tags.scm
@@ -0,0 +1,44 @@
+; Directives
+(class_path) @classpath
+
+(main) @entry
+
+(target) @target
+
+(library) @library
+
+(define) @define
+
+(macro) @macro
+
+(cmd) @command
+
+(cwd) @directory
+
+; Identifiers
+(type_name) @type
+
+(package) @package
+
+; Primitives
+(directory) @directory
+
+(file) @file
+
+(flag) @flag
+
+; Other
+(section) @section
+
+; Library details
+(library
+  (name) @library.name)
+
+; Define details
+(define
+  (define_key) @define.key)
+
+; Resource details
+(resource) @resource
+
+(resource_name) @resource.name
