1468c4a17f3e118486475598cf19b9d2f9a255b5
tong <tong@disktree.net>
2026-08-30 11:23:13 +0200

    chore(release): v0.2.4

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ endif
 
 LANGUAGE_NAME := tree-sitter-hxml
 HOMEPAGE_URL := https://github.com/tong/tree-sitter-hxml
-VERSION := 0.2.3
+VERSION := 0.2.4
 
 # repository
 SRC_DIR := src
--- src/parser.c
+++ src/parser.c
@@ -11281,7 +11281,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_hxml(void) {
     .metadata = {
       .major_version = 0,
       .minor_version = 2,
-      .patch_version = 3,
+      .patch_version = 4,
     },
   };
   return &language;
